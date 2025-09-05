7993b9f883f9648a8ffe29a24f6287293dcbadce
haewonwon <khcw1029@daum.net>
2025-09-05 17:55:27 +0900

    Feat: News 페이지에 Gallery 컴포넌트 통합 및 타입별 UI 렌더링 구현

--- src/components/news/index.ts
+++ src/components/news/index.ts
@@ -1,4 +1,6 @@
 export { default as BoardList } from './BoardList';
 export { default as BoardWrapper } from './BoardWrapper';
 export { default as BoardDetail } from './BoardDetail';
+export { default as GalleryList } from './GalleryList';
+export { default as GalleryWrapper } from './GalleryWrapper';
 export type { BoardItem } from './BoardList';
