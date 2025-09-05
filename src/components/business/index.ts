fc7b5cb042697ea054878cd52e22fefc0f7493d3
haewonwon <khcw1029@daum.net>
2025-09-05 14:16:12 +0900

    Feat: Business 페이지에 카페27b와 지역 활성화 사업 탭 추가 - LocalActivation과 Cafe27b 컴포넌트를 Business 페이지에 통합 - 4개 탭 완성: 도시재생, 행사기획, 카페27b, 지역활성화

--- src/components/business/index.ts
+++ src/components/business/index.ts
@@ -1,2 +1,4 @@
 export { default as UrbanRegenerationHub } from './UrbanRegenerationHub';
 export { default as EventsEducation } from './EventsEducation';
+export { default as Cafe27b } from './Cafe27b';
+export { default as LocalActivation } from './LocalActivation';
