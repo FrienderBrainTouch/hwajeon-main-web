ffe1735aaa773dd673b881ba02a60fde96cc9152
haewonwon <khcw1029@daum.net>
2025-09-08 09:29:07 +0900

    Feat: 공용 EventCalendar 컴포넌트 생성 - components/news에 재사용 가능한 EventCalendar 컴포넌트 추가 - 윤년 처리 포함한 완전한 월별 캘린더 구현 - 카테고리별 색상 표시 (#2C2E5A, #A692D1, #FFA484) - 카테고리 범례 옵션 제공

--- src/components/news/index.ts
+++ src/components/news/index.ts
@@ -1,6 +1,8 @@
 export { default as BoardList } from './BoardList';
 export { default as BoardWrapper } from './BoardWrapper';
 export { default as BoardDetail } from './BoardDetail';
+export { default as EventCalendar } from './EventCalendar';
 export { default as GalleryList } from './GalleryList';
 export { default as GalleryWrapper } from './GalleryWrapper';
 export type { BoardItem } from './BoardList';
+export type { EventCategory, EventData, MonthlyEventData } from './EventCalendar';
