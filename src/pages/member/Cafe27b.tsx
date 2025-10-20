959fc28f5adc72bdcca899ed2e12692ead7cdb2f
haewonwon <khcw1029@daum.net>
2025-10-20 17:40:16 +0900

    Fix: 빌드 에러 해결

--- src/pages/member/Cafe27b.tsx
+++ src/pages/member/Cafe27b.tsx
@@ -1,22 +1,9 @@
 import { Cafe27b as Cafe27bComponent } from '@/components/business';
-import {
-  mainImages,
-  menuItems,
-  productItems,
-  cateringServices,
-  mobileCafeServices,
-} from '@/components/business/data';
 
 function Cafe27b() {
   return (
     <div className="w-full py-8">
-      <Cafe27bComponent
-        mainImages={mainImages}
-        menuItems={menuItems}
-        productItems={productItems}
-        cateringServices={cateringServices}
-        mobileCafeServices={mobileCafeServices}
-      />
+      <Cafe27bComponent />
     </div>
   );
 }
