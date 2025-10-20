b25c4ae328c58d073ef3fc27be29afdc2eb7dac0
haewonwon <khcw1029@daum.net>
2025-10-20 11:55:12 +0900

    Fix: gitignore에test 확장자 추가, 안 쓰는 컴포넌트 정리

--- src/components/business/index.ts
+++ src/components/business/index.ts
@@ -1,9 +1,5 @@
-export { default as UrbanRegenerationHub } from './UrbanRegenerationHub';
-export { default as EventsEducation } from './EventsEducation';
-export { default as Cafe27b } from './Cafe27b';
-export { default as LocalActivation } from './LocalActivation';
-
 // 사업 안내 (수정 후)
+export { default as Cafe27b } from './Cafe27b';
 export { default as Business27b } from './Business27b';
 export { default as ContentsBusiness } from './ContentsBusiness';
 export { default as VillageCare } from './VillageCare';
