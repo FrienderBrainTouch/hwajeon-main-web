773ca1ce0401a81888384d58a2ebbb8fc22b2f3e
haewonwon <khcw1029@daum.net>
2025-10-22 10:19:07 +0900

    Fix: 깃허브 액션 환경변수 주입 에러 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
-const isPreview = process.env.PREVIEW === 'true';
-const prNumber = process.env.PR_NUMBER;
-const base = isPreview
-  ? `/hwajeon-main-web/pr-${prNumber}/` // 🔥 PR 프리뷰 전용 경로
-  : '/hwajeon-main-web/'; // 정식 배포 경로
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), ''); // ✅ .env 로드
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
-  },
-  base,
+  const isPreview = env.PREVIEW === 'true';
+  const pr = env.PR_NUMBER;
+  const base = isPreview && pr ? `/hwajeon-main-web/pr-${pr}/` : `/hwajeon-main-web/`;
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
+    },
+    base,
+  };
 });
