1af34f4e59acfa93bca5b664b112359436658808
Gapus <tjdwnd751@naver.com>
2026-03-19 16:14:02 +0900

    Reapply "20260319 버그 수정"
    This reverts commit b892c7ee4f8c8a111567ee4f48fd7c60e7ac88e5.

--- src/types/api/requests.ts
+++ src/types/api/requests.ts
@@ -8,11 +8,13 @@ import type { PostCategory } from './common';
  * @property {PostCategory} [postType] - 게시글 카테고리 필터 (선택적)
  * @property {number} [page] - 페이지 번호 (선택적)
  * @property {number} [size] - 페이지 크기 (선택적)
+ * @property {string} [sort] - 정렬 (예: createdAt,desc) (선택적)
  */
 export type GetPostsParams = {
   postType?: PostCategory;
   page?: number;
   size?: number;
+  sort?: string;
 };
 
 /**
